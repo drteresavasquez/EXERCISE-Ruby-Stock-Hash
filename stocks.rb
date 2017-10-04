8350a0c85f25c449532a915b2f6349dd56a65de1
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-04 15:34:19 -0500

    ruby stocks

---
+++ stocks.rb
@@ -0,0 +1,21 @@
+stock_dict = { 
+    'GM' => 'General Motors',
+    'CAT' => 'Caterpillar', 
+    'EK' => "Eastman Kodak" 
+}
+
+purchases = [ 
+    [ 'GM', 100, '10-sep-2001', 48 ],
+    [ 'CAT', 100, '1-apr-1999', 24 ],
+    [ 'GM', 200, '1-jul-1998', 56 ]
+]
+
+for item in purchases
+    stock_dict.each do |key, value|
+        if key == item[0]
+        puts "#{key}: #{value}"
+        puts "Total Paid: $#{(item[1]) * (item[3])}"
+        end
+    end
+end
+
