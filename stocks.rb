583b7282cb667fdfcd8773a9ec552e7a314959da
Dr. Teresa Vasquez <capewearingmama@Drs-MacBook-Pro.local>
2017-10-04 15:56:15 -0500

    added total price calcs

--- stocks.rb
+++ stocks.rb
@@ -13,9 +13,22 @@ purchases = [
 for item in purchases
     stock_dict.each do |key, value|
         if key == item[0]
-        puts "#{key}: #{value}"
-        puts "Total Paid: $#{(item[1]) * (item[3])}"
+            puts "#{key}: #{value}"
+            puts "Total Paid: $#{(item[1]) * (item[3])}"
         end
     end
 end
 
+stuff = Hash.new
+for item in purchases
+    stock_dict.each do |key, value|
+        if key == item[0]
+            if stuff.has_key?(key) == true
+                stuff[key] = (stuff[key]) + (item[1]) * (item[3])
+            else 
+                stuff[key] = (item[1]) * (item[3])
+            end
+        end
+    end
+end
+puts stuff
